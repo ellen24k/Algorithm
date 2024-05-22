d66a61381bdcbbbbf8bdebd8812321d7ab83b26f
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-05-23 00:31:13 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ C/프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+char* solution(const char* arr[], size_t arr_len) {
+    char* answer = (char*)malloc(arr_len+1);
+
+    for (int i = 0; i < arr_len;i++) {
+        answer[i] = *arr[i];
+    }
+    answer[arr_len] = '\0';
+    return answer;
+}
