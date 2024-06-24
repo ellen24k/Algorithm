e1d40c4313cd757b8ee7d194ee9ccc6eebed6b75
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-06-24 09:32:39 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ C/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+int solution(int n) {
+    int answer = 0;
+    int length = 0;
+    int num = n;
+    
+    while(num>0){
+        length++;
+        num /= 3;
+    }
+    
+    for(int i = 1; i<=length; i++){
+        answer += (n%3)*pow(3,length-i);
+        n /= 3;
+    }
+    
+    return answer;
+}
