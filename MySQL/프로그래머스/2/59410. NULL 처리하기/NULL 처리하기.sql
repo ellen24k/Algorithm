fea766311f3de5790dadbc3de3757227afbc48f1
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-06-24 22:58:45 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ MySQL/프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,2 +1,2 @@
 -- 코드를 입력하세요
-SELECT animal_type, IFNULL(name,"No name") as name, sex_upon_intake from animal_ins order by animal_id;
+SELECT animal_type, ifnull(name,"No name") as name, sex_upon_intake from animal_ins order by animal_id;
