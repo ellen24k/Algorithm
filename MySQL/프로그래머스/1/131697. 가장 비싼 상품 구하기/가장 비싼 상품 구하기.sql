f3e07d4e2d332fab6a79149e0f2f42c30d1d1e3a
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-06-26 00:01:06 +0900

    Update 가장 비싼 상품 구하기.sql
    max() 사용

--- MySQL/프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
+++ MySQL/프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
@@ -1,2 +1,3 @@
 -- 코드를 입력하세요
-SELECT price as MAX_PRICE from product order by price desc limit 1;
+# SELECT price as MAX_PRICE from product order by price desc limit 1;
+select max(price) as MAX_PRICE from product;
