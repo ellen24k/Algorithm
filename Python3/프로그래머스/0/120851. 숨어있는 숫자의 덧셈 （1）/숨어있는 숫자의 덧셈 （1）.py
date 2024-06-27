eb4251740dd09244b0bf0ecf5340ee529c77efb0
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-06-27 11:37:08 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- Python3/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ Python3/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -2,6 +2,6 @@ def solution(my_string):
     str_list = list(my_string)
     answer = 0
     for i in str_list:
-        if i.isalpha() == False:
+        if i.isdigit():
             answer += int(i)
     return answer
