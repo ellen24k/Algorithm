109672baabbf1e478dc64f7188aec353cede3cb2
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-07-01 11:40:26 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = list()
+    
+    for i in range(0,len(my_string)):
+        if my_string[i].islower():
+            answer.append(my_string[i].upper())
+        else:
+            answer.append(my_string[i].lower())
+    return ''.join(answer)
