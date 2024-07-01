ce63209ce2971a6ce62d4dddd2fd0a563ff7c9be
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-07-01 11:41:55 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ Python3/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,9 +1,10 @@
 def solution(my_string):
-    answer = list()
+    # answer = list()
     
-    for i in range(0,len(my_string)):
-        if my_string[i].islower():
-            answer.append(my_string[i].upper())
-        else:
-            answer.append(my_string[i].lower())
-    return ''.join(answer)
+    # for i in range(0,len(my_string)):
+        # if my_string[i].islower():
+            # answer.append(my_string[i].upper())
+        # else:
+            # answer.append(my_string[i].lower())
+    # return ''.join(answer)
+    return my_string.swapcase()
