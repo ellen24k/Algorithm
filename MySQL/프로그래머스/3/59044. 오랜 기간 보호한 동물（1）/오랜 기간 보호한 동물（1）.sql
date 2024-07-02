2b068c6aa1f604a7c0ea5e1fdd8487b2313c72eb
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-07-02 15:30:48 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,16 @@
+-- 코드를 입력하세요
+select
+    ins.NAME, ins.DATETIME
+from    
+    ANIMAL_INS as ins
+left join
+    ANIMAL_OUTS as outs
+on
+    ins.ANIMAL_ID = outs.ANIMAL_ID
+where 
+    outs.ANIMAL_ID is null
+order by
+    ins.DATETIME
+limit 3;
+
+    
