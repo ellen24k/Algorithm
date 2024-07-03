e51796a3474beb3bc0158c42321036b6af9d60e0
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-07-03 12:02:05 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- C/프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.c
+++ C/프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int* solution(int n, int numlist[], size_t numlist_len) {
-    int* answer = (int*)malloc(numlist_len);
+    int* answer = (int*)malloc(sizeof(int) * numlist_len);
     
     for(int i = 0, count = 0; i<numlist_len;i++){
         if (numlist[i] % n == 0) {
