89c048d8806f3875f47fb836f5ee92b6c987be98
ellen24k <161318146+ellen24k@users.noreply.github.com>
2024-07-10 12:34:59 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- C/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.c
+++ C/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.c
@@ -7,12 +7,12 @@ int compare(int* a, int* b) { //오름차순
 }
 
 int* solution(int num_list[], size_t num_list_len) {    
-    int* answer = (int*)malloc(num_list_len-5);
+    int* answer = (int*)malloc(sizeof(int) * num_list_len-5);
     qsort(num_list, num_list_len, sizeof(int), compare);
     
     for (int i = 0; i < num_list_len-5; i++) {
         answer[i] = num_list[i+5];
-        printf("%d ",answer[i]);
+        // printf("%d ",answer[i]);
     }
     
     return answer;
