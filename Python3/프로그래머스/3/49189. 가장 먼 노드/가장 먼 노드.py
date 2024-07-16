c85fe7b797d1611de1015c135005fa9539bcc0b3
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-07-16 11:30:52 +0900

    [level 3] Title: 가장 먼 노드, Time: 72.49 ms, Memory: 20.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def bfs(graph, node, visited):
+    queue = deque([[node, 0]])
+    visited[node] = 1
+    # print(queue)
+    
+    while queue:
+        [v,c] = queue.popleft()
+        # print(v, c, graph[v])
+        
+        for index in graph[v]:
+            if not (visited[index]):
+                queue.append([index,c+1])
+                visited[index] = c+1
+    return visited
+
+def solution(n, edge):
+    
+    graph = [[] for _ in range(n+1)]
+    visited = [0] * (n+1)
+    # print(edge)
+    
+    for i in edge:
+        graph[i[0]].append(i[1])
+        graph[i[1]].append(i[0])
+    # print(graph)
+    
+    distance = bfs(graph,1,visited)
+    # print(distance)
+    return distance.count(max(distance))
