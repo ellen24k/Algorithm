340baf685ce4eb7542d1dec03f587cedf1a53905
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-07-16 22:56:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.79 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,15 +1,15 @@
 from collections import deque
 
 def bfs(graph,n,m, visited):
-    queue = deque([[0, 0]])
+    queue = deque([[0, 0]]) # [0,0],[1,1]
     visited[0][0] = 1
     
-    x_weight = [0,1,0,-1] #하->우->상->좌 순으로 이동하기 위한 가중치
+    x_weight = [0,1,0,-1] # 하->우->상->좌 순으로 이동하기 위한 가중치
     y_weight = [-1,0,1,0]
     
     while queue:
         [cur_x,cur_y] = queue.popleft()
-        # print(cur_x, cur_y, graph[cur_x][cur_y])
+        # print(cur_x, cur_y, graph[cur_x][cur_y]) # 주석처리
         for i in range(4):
             temp_x = cur_x + x_weight[i] # 이동하려는 좌표
             temp_y = cur_y + y_weight[i]
@@ -19,16 +19,22 @@ def bfs(graph,n,m, visited):
                     if not (visited[temp_x][temp_y]): # 지나간 적 없는 위치인지 확인
                         queue.append([temp_x,temp_y])
                         visited[temp_x][temp_y] = visited[cur_x][cur_y] + 1
+                        if (visited[-1][-1] != 0) : # 상대 팀 진영에 도착 시 반복을 종료해 성능향상
+                            queue.clear()
+                            break
     
-    # for r in visited: # 이동 경로 확인용
-        # print(r)
+            # for r in visited: # 이동 경로 확인용
+                # print(r)
+            # print()
     
     return visited
 
 
 def solution(maps):
     visited = [[0 for _ in range(len(maps[0]))] for _ in range(len(maps))]
-    print(visited)
+    # for r in visited: # 이동 경로 확인용
+        # print(r) #
+    # print() #
     count = bfs(maps, len(maps), len(maps[0]), visited)
-    print(count[-1][-1])
+    # print(count[-1][-1])
     return count[-1][-1] if count[-1][-1] else -1
