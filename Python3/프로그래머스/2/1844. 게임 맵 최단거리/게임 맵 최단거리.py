63bcd5b9f129cc247489939ebbdfc3863e24fe75
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-07-16 19:36:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.89 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+def bfs(graph,n,m, visited):
+    queue = deque([[0, 0]])
+    visited[0][0] = 1
+    
+    x_weight = [0,1,0,-1] #하->우->상->좌 순으로 이동하기 위한 가중치
+    y_weight = [-1,0,1,0]
+    
+    while queue:
+        [cur_x,cur_y] = queue.popleft()
+        # print(cur_x, cur_y, graph[cur_x][cur_y])
+        for i in range(4):
+            temp_x = cur_x + x_weight[i] # 이동하려는 좌표
+            temp_y = cur_y + y_weight[i]
+            
+            if (temp_x>-1 and temp_x<n and temp_y>-1 and temp_y<m): # 맵 내부에 위치한 좌표인지 확인
+                if graph[temp_x][temp_y] != 0: # 벽이 아닌지 확인
+                    if not (visited[temp_x][temp_y]): # 지나간 적 없는 위치인지 확인
+                        queue.append([temp_x,temp_y])
+                        visited[temp_x][temp_y] = visited[cur_x][cur_y] + 1
+    
+    # for r in visited: # 이동 경로 확인용
+        # print(r)
+    
+    return visited
+
+
+def solution(maps):
+    visited = [[0 for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    print(visited)
+    count = bfs(maps, len(maps), len(maps[0]), visited)
+    print(count[-1][-1])
+    return count[-1][-1] if count[-1][-1] else -1
+#todo list index 범위 확인
