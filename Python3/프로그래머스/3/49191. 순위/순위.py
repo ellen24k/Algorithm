5a0193202a40f7ab06e34d3a4daf8d7bb9761153
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-07-23 20:52:34 +0900

    [level 3] Title: 순위, Time: 218.95 ms, Memory: 10.5 MB -BaekjoonHub

--- Python3/프로그래머스/3/49191. 순위/순위.py
+++ Python3/프로그래머스/3/49191. 순위/순위.py
@@ -4,12 +4,14 @@ def dfs(game_results, player, trace_code, trace_index):
     for new_player in range(len(game_results)):
         # print(player+1,new_player+1)
         # 추적 코드에 맞는 플레이어의 대진 결과 반영
-        if game_results[player][new_player] == trace_code and new_player not in trace_index:
+        if (
+            game_results[player][new_player] == trace_code 
+            and new_player not in trace_index
+        ):
             trace_index.append(new_player)
             dfs(game_results, new_player, trace_code, trace_index)
             # print(player + 1, "trace 결과:", trace_index)
-
-
+ 
 def solution(n, results):
     game_results = [[0 for _ in range(n)] for _ in range(n)]
     # for player_num in range(1,n+1): # 경기결과 초기화
