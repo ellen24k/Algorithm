25406b9a3ea5b30f7dea96485e6519d12ff668a9
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-07-24 21:34:22 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/42862. 체육복/체육복.py
+++ Python3/프로그래머스/1/42862. 체육복/체육복.py
@@ -1,4 +1,5 @@
 def solution(n, lost, reserve):
+    # lost : 체육복을 빌려야 하는 학생 / reserve : 체육복을 빌려줄 수 있는 학생
     lost, reserve = list(set(lost) - set(reserve)), list(set(reserve) - set(lost))
     # print("l:", lost, "r:", reserve)
     answer = n - len(lost)
