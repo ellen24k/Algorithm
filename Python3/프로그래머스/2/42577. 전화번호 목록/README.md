2cbdf1b88a534644e7c5ee816e2928b253dbd5b3
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-07-24 21:14:10 +0900

    [level 2] Title: 전화번호 목록, Time: 455.29 ms, Memory: 41.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/42577. 전화번호 목록/README.md
+++ Python3/프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 41.4 MB, 시간: 456.24 ms
+메모리: 41.3 MB, 시간: 455.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 24일 13:18:57
+2024년 07월 24일 21:14:08
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -9,11 +9,11 @@ def solution(phone_book): # set 이용 성공
 
 # def solution(phone_book): # 효율성 시간초과
     # phone_book.sort(key=lambda x: len(x))
-#     # print(phone_book)
+#     print(phone_book)
     
 #     for i in range(len(phone_book)-1):
 #         for j in range(i+1, len(phone_book)):
-#             # print("i:",phone_book[i],"j:",phone_book[j])
+#             print("i:",phone_book[i],"j:",phone_book[j])
 #             if phone_book[i] in phone_book[j][:len(phone_book[i])]:
 #                 return False
     
