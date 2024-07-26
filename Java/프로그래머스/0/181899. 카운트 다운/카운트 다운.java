739a15655f839d4fdb28a45bdbd4c5db64bd20f7
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-07-26 21:00:17 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[start_num - end_num + 1];
+        
+        for(int i = 0, j = start_num; i < start_num - end_num + 1; i++, j--){
+            answer[i] = j;
+        }
+        return answer;
+    }
+}
