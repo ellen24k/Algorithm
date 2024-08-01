409d0776a447473eb6d4549b56257979c95d0f2d
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-08-01 11:44:23 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.58 MB -BaekjoonHub

---
+++ C/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int solution(int num_list[], size_t num_list_len) {
+    int sum = 0;
+    int product = 1;
+    
+    for (size_t i = 0; i < num_list_len; i++) {
+        sum += num_list[i];
+        product *= num_list[i];
+    }
+    
+    if (product < sum * sum) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
