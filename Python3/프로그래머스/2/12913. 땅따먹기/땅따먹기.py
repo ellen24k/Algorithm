efac0888b3f483b9f4c45f9d3bc567d3f968fe00
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-08-02 11:56:20 +0900

    [level 2] Title: 땅따먹기, Time: 385.82 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,8 @@
+def solution(land):
+
+    for i in range(1,len(land)):
+        for j in range(4):
+            land[i][j] += max((land[i-1][index] for index in range(4) if index != j))
+        # print(i,land[i])
+        
+    return max(land[-1])
