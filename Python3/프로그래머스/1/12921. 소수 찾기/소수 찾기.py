14b6f8dea354fc5f70d4467723b5ef1b8b00db97
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-08-04 12:12:47 +0900

    [level 1] Title: 소수 찾기, Time: 336.67 ms, Memory: 113 MB -BaekjoonHub

--- Python3/프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ Python3/프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,13 +1,23 @@
-def prime_count(num):
-    count = 0
-    for i in range(2,num + 1):
-        is_prime = True
-        for j in range(2,int(i**0.5) + 1):
-            if i%j == 0:
-                is_prime = False
-                break
-        if is_prime: count += 1
-    return count
+def solution(n): # 에라스토테네스의 체
+    num=set(range(2,n+1))
 
-def solution(n):
-    return prime_count(n)
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
+
+
+
+# def prime_count(num): # 성공
+#     count = 0
+#     for i in range(2,num + 1):
+#         is_prime = True
+#         for j in range(2,int(i**0.5) + 1):
+#             if i%j == 0:
+#                 is_prime = False
+#                 break
+#         if is_prime: count += 1
+#     return count
+
+# def solution(n):
+#     return prime_count(n)
