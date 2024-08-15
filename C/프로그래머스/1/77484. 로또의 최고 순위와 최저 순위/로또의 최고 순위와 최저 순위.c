01bd87ff19b7865061bc6ecdf93b62ca0b600722
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-08-15 10:31:52 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ C/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int lottos[], size_t lottos_len, int win_nums[], size_t win_nums_len) {
+    int* answer = (int*)malloc(2);
+    int same_num_count = 0;
+    int zero_count = 0;
+    
+    for (int i = 0; i < 6; i++) {
+        if (lottos[i] != 0) {
+            for (int j = 0; j < 6; j++) {
+                if (lottos[i] == win_nums[j]) same_num_count++;
+            }
+        } else zero_count++;
+    }
+    
+    answer[0] = (same_num_count == 0 && zero_count == 0) ? 6 : 7 - same_num_count - zero_count;
+    answer[1] = (same_num_count == 0) ? 6 : 7 - same_num_count;
+    
+    return answer;
+}
