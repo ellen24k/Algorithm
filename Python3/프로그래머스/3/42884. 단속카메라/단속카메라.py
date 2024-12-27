6f6e24ad5b37d0eac6daaf63537261c3ce8fc6ec
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-12-27 17:17:14 +0900

    [level 3] Title: 단속카메라, Time: 1.06 ms, Memory: 10.7 MB -BaekjoonHub

--- Python3/프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ Python3/프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,22 +1,36 @@
-from collections import defaultdict
 def solution(routes):
     routes.sort(key= lambda route: route[1])
-    cctv_s = []
+    last_cctv = -30001
     cnt = 0
     
     for route in routes:
-        already_coverable = False
-    
-        for cctv in cctv_s[::-1]:
-            if route[0] <= cctv <= route[1]: 
-                already_coverable = True
-                break
-        
-        if not already_coverable:
-            cctv_s.append(route[1])
+        if not (route[0] <= last_cctv <= route[1]):
+            last_cctv = route[1]
             cnt += 1
             
     # print(routes)
     return cnt
+    
+# from collections import defaultdict
+
+# def solution(routes):
+#     routes.sort(key= lambda route: route[1])
+#     cctv_s = []
+#     cnt = 0
+    
+#     for route in routes:
+#         already_coverable = False
+    
+#         for cctv in cctv_s[::-1]:
+#             if route[0] <= cctv <= route[1]: 
+#                 already_coverable = True
+#                 break
+        
+#         if not already_coverable:
+#             cctv_s.append(route[1])
+#             cnt += 1
+            
+#     # print(routes)
+#     return cnt
     
     
