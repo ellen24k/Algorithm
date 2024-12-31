3bb68113dc3f1e779e4dca3f6a9024d704a63f5a
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-12-31 10:50:03 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- C++/프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
+++ C++/프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -7,5 +7,5 @@ using namespace std;
 int solution(vector<int> numbers) {
     int size = numbers.size();
     sort(numbers.begin(),numbers.end());
-    return (numbers[0]*numbers[1] > numbers[size-1]*numbers[size-2]) ? numbers[0]*numbers[1] : numbers[size-1]*numbers[size-2];
+    return max(numbers[0]*numbers[1],numbers[size-1]*numbers[size-2]);
 }
