0521e0313d4c3c3d651d4430f7b3cf311e91bbf8
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2025-01-01 11:58:00 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 9.96 MB -BaekjoonHub

--- Python3/프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ Python3/프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,22 +1,36 @@
+from collections import defaultdict
+
 def solution(array):
-    array.sort()
+    freq = defaultdict(int)
+    for elem in array: freq[elem] += 1
     
-    answer = array[0]
-    answer_count = array.count(array[0])
-    not_only = False
+    sorted_arr = sorted(set(array), key= lambda elem: -freq[elem])
+    print(sorted_arr)
     
-    for i in range(0,len(array)-1):
-        if array[i] != array[i+1]:
-            if answer_count < array.count(array[i+1]):
+    if len(sorted_arr) > 1:
+        return sorted_arr[0] if freq[sorted_arr[0]] > freq[sorted_arr[1]] else -1
+    return sorted_arr[0]
+
+    
+# def solution(array):
+#     array.sort()
+    
+#     answer = array[0]
+#     answer_count = array.count(array[0])
+#     not_only = False
+    
+#     for i in range(0,len(array)-1):
+#         if array[i] != array[i+1]:
+#             if answer_count < array.count(array[i+1]):
                 
-                answer_count = array.count(array[i+1])
-                answer = array[i+1]
-                not_only = False
+#                 answer_count = array.count(array[i+1])
+#                 answer = array[i+1]
+#                 not_only = False
                 
-            elif answer_count == array.count(array[i+1]):
-                not_only = True
+#             elif answer_count == array.count(array[i+1]):
+#                 not_only = True
                 
-    if not_only:
-        return -1
-    else:
-        return answer
+#     if not_only:
+#         return -1
+#     else:
+#         return answer
