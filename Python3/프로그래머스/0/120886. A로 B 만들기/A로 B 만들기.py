2c9dd6b570d7e21a2a26878691d7d246cb785c56
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2025-01-22 09:45:06 +0900

    [level 0] Title: A로 B 만들기, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ Python3/프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,6 +1,9 @@
 def solution(before, after):
-    bef_set = set(before)
-    aft_set = set(after)
-    for before_chr in bef_set:
-        if before.count(before_chr) != after.count(before_chr): return 0
-    return 1
+    return (sorted(list(before)) == sorted(list(after))) * 1
+
+# def solution(before, after):
+#     bef_set = set(before)
+#     aft_set = set(after)
+#     for before_chr in bef_set:
+#         if before.count(before_chr) != after.count(before_chr): return 0
+#     return 1
