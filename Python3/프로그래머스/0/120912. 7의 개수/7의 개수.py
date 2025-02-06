0fe9f3ec533138fdbd54706dc40938f461f42b32
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2025-02-06 10:14:57 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ Python3/프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,7 +1,10 @@
 def solution(array):
-    answer = 0
-    for elem in array:
-        for chr in str(elem):
-            if chr == '7':
-                answer += 1
-    return answer
+    return str(array).count('7')
+
+# def solution(array):
+#     answer = 0
+#     for elem in array:
+#         for chr in str(elem):
+#             if chr == '7':
+#                 answer += 1
+#     return answer
