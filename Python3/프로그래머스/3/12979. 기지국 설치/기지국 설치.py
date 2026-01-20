b47984e7013da92c08c6cbf91619b55a125ae161
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-01-20 09:59:03 +0900

    [level 3] Title: 기지국 설치, Time: 9.63 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,17 @@
+def solution(n, stations, w):
+    answer = 0
+    uncovered = 1
+    station_idx = -len(stations)
+    stations_covering_from = [range(station-w, station+w+1) for station in stations]
+    
+    while (uncovered <= n):
+        if (station_idx < -1 and stations[station_idx] + w < uncovered): station_idx += 1
+        if (uncovered in stations_covering_from[station_idx]):
+            uncovered = stations[station_idx] + w + 1
+        else:
+            answer += 1
+            uncovered += 2 * w + 1
+        # print(answer, uncovered, stations_covering_from[station_idx])
+        
+
+    return answer
