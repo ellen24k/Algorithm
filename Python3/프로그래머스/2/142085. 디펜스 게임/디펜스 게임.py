4f12a8a0540e1984a63f61bdde989f0a146e7d69
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-02-12 10:44:20 +0900

    [level 2] Title: 디펜스 게임, Time: 622.20 ms, Memory: 45 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -0,0 +1,19 @@
+from heapq import heappush, heappop
+
+def solution(n, k, enemy):
+    prev_n_min_heap = [] 
+    i = 0
+    
+    for i in range(len(enemy)):
+        heappush(prev_n_min_heap, enemy[i])
+        if (k):
+            i += 1
+            k -= 1
+        else:
+            prev_min = heappop(prev_n_min_heap)
+            if (n >= prev_min):
+                i += 1
+                n -= prev_min
+            else: break
+
+    return i
