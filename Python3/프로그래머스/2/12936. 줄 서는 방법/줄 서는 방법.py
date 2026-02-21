593fc8058dfaec25a6e9ed56bac3773873858461
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-02-21 09:58:56 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -0,0 +1,17 @@
+import math
+from collections import deque
+
+def solution(n, k):
+    num = [i for i in range(1, n + 1)]
+    answer = []
+    
+    for i in range(n, 0, -1):
+        fact_n = math.factorial(i - 1)
+        num_idx = 0
+        while (fact_n < k):
+            k -= fact_n
+            num_idx += 1
+        answer.append(num[num_idx])
+        del num[num_idx]
+    
+    return answer
