40fd7c7ef176634f40ed5dfcb69a61b0e30db6eb
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-07-13 20:34:26 +0900

    [level 2] Title: 할인 행사, Time: 64.14 ms, Memory: 15.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ Python3/프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -2,20 +2,18 @@ from collections import defaultdict
 
 def solution(want, number, discount):
     answer = 0
-    discount10days = defaultdict(int)
-    wantDict = {want[i]: number[i] for i in range(len(want))}
-    
-    for i in range(9): discount10days[discount[i]] += 1
-    
-    for i in range(9, len(discount)):
-        discount10days[discount[i]] += 1
-        
-        if (discount[i] in wantDict and discount10days[discount[i]] == wantDict[discount[i]]):
-            # print(f'trigger: {discount[i]}')
-            for k, v in wantDict.items():
-                if (discount10days[k] != v): break
-            else: answer += 1
-            
-        # print(discount10days)
-        discount10days[discount[i - 9]] -= 1
+    want_discount = dict(zip(want, number))
+
+    # 10일 할인을 받을 수 있는 범위에서 연속 할인 가능한지 확인
+    for i in range(0,len(discount) - 9):
+        temp_discount = defaultdict(int)
+
+        for j in range(10):
+            temp_discount[discount[i + j]] += 1
+            # 10일 간 원하는 모든 제품을 할인 받을 수 없는 경우
+            if discount[i + j] not in want_discount or temp_discount[discount[i + j]] > want_discount[discount[i+j]]: break
+        # 원하는 모든 할인을 받을 수 있는 경우
+        else:
+            answer += 1
+
     return answer
