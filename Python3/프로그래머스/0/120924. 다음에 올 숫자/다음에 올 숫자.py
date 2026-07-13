d145b0a78cb256643427b72be36e627177892817
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-07-13 14:51:45 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- Python3/프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ Python3/프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,5 +1,5 @@
 def solution(common):
-    d = common[1] - common[0]
-    r = common[1] // common[0] if common[0] != 0 else 0
-    if (common[-1] - common[-2] == d): return common[-1] + d
-    else: return common[-1] * r
+    if common[1] - common[0] == common[2] - common[1]: # 등차
+        return common[-1] + (common[1] - common[0])
+    else: # 등비
+        return common[-1] * (common[1] // common[0])
