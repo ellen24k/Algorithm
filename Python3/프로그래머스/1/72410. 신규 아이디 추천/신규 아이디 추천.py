d49582ec9f7c2a86a6e4fd8aabeded336b5f400b
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-07-16 12:34:56 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.14 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ Python3/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,18 +1,73 @@
-import re
-
 def solution(new_id):
-    answer = new_id.lower() # step 1
-    answer = re.sub('[^\w\-.]', '', answer) # step 2
-    answer = re.sub('[.]+', '.', answer) # step 3
-    answer = re.sub('^[.]|[.]$', '', answer) # step 4
-    if answer == '': answer = 'a' # step 5
-    # step 6
-    if len(answer) >= 16: answer = answer[:15] if answer[14] != '.' else answer[:14]
-    # step 7
-    answer = answer if len(answer) > 2 else answer + ''.join([answer[-1] for _ in range(3-len(answer))])
+    answer = ''
+    # step 1
+    step_1 = []
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for c in new_id:
+        if c in upper: # 리스트 컴프리헨션
+            step_1.append(lower[upper.index(c)])
+        else:
+            step_1.append(c)
+    # print(step_1)
+    
+    # step_2
+    restriction_2 = 'abcdefghijklmnopqrstuvwxyz0123456789-_.' # 정규식
+    step_2 = [c for c in step_1 if c in restriction_2]
+    # print(step_2)
+    
+    # step_3
+    # step_3 = ''.join(step_2)
+    # step_3.replace('..', '.') # ... 등 처리 필요
+    continuous_dot = False
+    step_3 = []
+    for idx, c in enumerate(step_2):
+        if c == '.':
+            # if idx != 0: 
+            continuous_dot = True
+        else:
+            if continuous_dot:
+                step_3.append('.')
+                continuous_dot = False
+            step_3.append(c)
+    # if continuous_dot:
+        # step_3.append('.') # 마지막에 .으로 끝난 경우?
+    # print(step_3)
+    
+    # step_4
+    if len(step_3) > 1:
+        if step_3[0] == '.':
+            step_3 = step_3[1:]
+        if step_3[-1] == '.':
+            step_3 = step_3[:len(step_3)-1]
+    else:
+        if step_3 and step_3[0] == '.':
+            step_3 = []
+            
+    
+#     if step_3[0] == '.':
+#         if len(step_3) > 1:
+#             step_3 = step_3[1:]
+#         else:
+#             step_3 = []
+#     if step_3[-1] == '.':
+#         step_3 = step_3[:len(step_3-1)]
+    
+    # step_5
+    if not step_3: 
+        step_3 = ['a']
         
-    # print(answer)
-    return answer
+    # step_6
+    step_6 = step_3[:15]
+    if step_6[-1] == '.':
+        step_6 = step_6[:14]
+    # print(len(step_6))
 
-# re.sub(pattern, replace, text) >> text 중 pattern에 해당하는 부분을 replace로 치환.
-# \w >> 'alphanumeric' + '_'
+    # step_7
+    while len(step_6) <= 2:
+        step_6.append(step_6[-1])
+    # print(step_6)
+    
+            
+    
+    return ''.join(step_6)
