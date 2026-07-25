c30ccefbca825a93e41c48a7996cf03113341c41
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-07-25 15:40:42 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.06 ms, Memory: 59.2 MB -BaekjoonHub

--- Kotlin/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
+++ Kotlin/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
@@ -2,10 +2,6 @@ class Solution {
     fun solution(my_string: String, letter: String): String {
         val char_letter = letter[0]
         
-        return buildString(my_string.length) {
-            my_string.forEach { my_char ->
-                if (my_char != char_letter) append(my_char)
-            }
-        }
+        return my_string.filterNot { it == char_letter }
     }
 }
