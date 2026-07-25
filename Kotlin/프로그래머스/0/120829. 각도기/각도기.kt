c071f3c8c286292dcf73c85d7339c1c9c97ff6ce
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-07-25 16:02:52 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 59.8 MB -BaekjoonHub

--- Kotlin/프로그래머스/0/120829. 각도기/각도기.kt
+++ Kotlin/프로그래머스/0/120829. 각도기/각도기.kt
@@ -1,8 +1,8 @@
 class Solution {
-    fun solution(angle: Int): Int = when {
-        angle < 90 -> 1
-        angle == 90 -> 2
-        angle < 180 -> 3
+    fun solution(angle: Int): Int = when(angle) {
+        in 1..89 -> 1
+        90 -> 2
+        in 91..179 -> 3
         else -> 4
     }
 }
