3fa49d8ad47626a40bdadbd1db0ded0127d2324c
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-07-25 11:22:38 +0900

    [level 0] Title: 배열의 평균값, Time: 9.89 ms, Memory: 63.9 MB -BaekjoonHub

--- Kotlin/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.kt
+++ Kotlin/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(numbers: IntArray): Double = if (numbers.size > 0) numbers.sum().toDouble() / numbers.size else 0.toDouble()
+    fun solution(numbers: IntArray): Double = numbers.average()
 }
