30c17fd80fc72dc4a29692ccba40a2e817433e3f
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-07-26 10:07:45 +0900

    [level 0] Title: 배열 자르기, Time: 6.87 ms, Memory: 64.1 MB -BaekjoonHub

--- Kotlin/프로그래머스/0/120833. 배열 자르기/배열 자르기.kt
+++ Kotlin/프로그래머스/0/120833. 배열 자르기/배열 자르기.kt
@@ -1,3 +1,7 @@
 class Solution {
-    fun solution(numbers: IntArray, num1: Int, num2: Int) = numbers.sliceArray(num1..num2)
+    fun solution(numbers: IntArray, num1: Int, num2: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        for (i in num1..num2) answer = answer.plus(numbers[i])
+        return answer
+    }
 }
