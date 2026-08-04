76948425e562789bb87bd00587090658025bbe55
Taeyoung Kim <161318146+ellen24k@users.noreply.github.com>
2026-08-04 10:00:03 +0900

    [level 1] Title: 직사각형 별찍기, Time: 124.74 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+
+    for(j in 1..b){
+        for(i in 1..a){
+            print('*')
+        }
+        println(' ')
+    }
+}
+
